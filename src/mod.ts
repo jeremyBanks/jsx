4a5c7a80ea4a144da5c67ef371168635f10f20d3
Jeremy Banks <_@jeremy.ca>
2025-02-22 06:24:53 +0000

    m

--- src/mod.ts
+++ src/mod.ts
@@ -15,25 +15,60 @@ export class JsxElement {
   ) {}
 
   toString(opts?: JsxStringOpts): string {
-    return `${
-      this.type
-        ? `<${this.type}${
-          Object.entries(this.attributes).map(([name, value]) => {
-            let buffer = " ";
+    const body = this.children.map((child) => {
+      if (child instanceof JsxElement) {
+        return child.toString({ ...opts });
+      } else if (typeof child === "string") {
+        return textAsChildOfTag(child, this.type);
+      } else {
+        throw new TypeError(`unexpected child type`);
+      }
+    }).join("");
 
-            buffer += name;
+    if (!this.type) {
+      return body;
+    }
 
-            if (typeof value === "string") {
-              buffer += `=${textAsAttributeValue(value)}`;
-            } else if (value !== true) {
-              throw new TypeError(`unexpected attribute value type`);
-            }
+    if (
+      !body && [
+        "area",
+        "base",
+        "br",
+        "col",
+        "embed",
+        "hr",
+        "img",
+        "input",
+        "link",
+        "meta",
+        "param",
+        "source",
+        "track",
+        "wbr",
+      ].includes(this.type.toLowerCase())
+    ) {
+      return `<${this.type}/>`;
+    }
 
-            return buffer;
-          }).join("")
-        }>`
-        : ""
-    }${
+    if (this.type == "!--") {
+      return `<!--${body}-->`;
+    }
+
+    return `<${this.type}${
+      Object.entries(this.attributes).map(([name, value]) => {
+        let buffer = " ";
+
+        buffer += name;
+
+        if (typeof value === "string") {
+          buffer += `=${textAsAttributeValue(value)}`;
+        } else if (value !== true) {
+          throw new TypeError(`unexpected attribute value type`);
+        }
+
+        return buffer;
+      }).join("")
+    }>${
       this.children.map((child) => {
         if (child instanceof JsxElement) {
           return child.toString({ ...opts });
@@ -43,6 +78,6 @@ export class JsxElement {
           throw new TypeError(`unexpected child type`);
         }
       }).join("")
-    }${this.type ? `<${this.type}>` : ""}`;
+    }</${this.type}>`;
   }
 }
