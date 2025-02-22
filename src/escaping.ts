c1a1e56d58f2eb877fb022dfcc620ed84fec62d2
Jeremy Banks <_@jeremy.ca>
2025-02-22 05:31:29 +0000

    m

--- src/escaping.ts
+++ src/escaping.ts
@@ -1,31 +1,83 @@
-import { unimplemented } from "jsr:@std/assert/unimplemented";
+export function textAsAttribute(text: string): string {
+  let encoded = text;
 
-export function textForInHtmlAttribute(text: string, opts = {
-  escapedQuotes: `"'` as `"'` | `"` | `'`
-}): string {
-  return unimplemented();
+  encoded = encoded.replaceAll("\0", "\uFFFD");
+
+  encoded = encoded.replace(/&(?=[a-zA-Z0-9]+;|[a-zA-Z0-9]*$)/ug, "&amp;");
+
+  if (encoded.length > 0 && !encoded.match(/[\t\n\f &>"'<=`]/)) {
+    return encoded;
+  } else if (!encoded.includes('"')) {
+    return `"${encoded}"`;
+  } else if (!encoded.includes("'")) {
+    return `'${encoded}'`;
+  } else {
+    return `"${encoded.replaceAll('"', "&#34;")}"`;
+  }
 }
 
 export function textAsChildOfTag(text: string, tag?: string): string {
+  tag = tag?.toLowerCase();
+
   let encoded = text;
 
-  if (tag == 'script') {
-    return encoded = encoded.replaceAll('</script', '<\/script');
+  encoded = encoded.replaceAll("\0", "\uFFFD");
+
+  if (tag == "script") {
+    // This is safe if the type is JSON or JavaScript because the only
+    // location where this sequence can occur is in a string literal,
+    // where this will be valid and equivalent, or a comment.
+    encoded = encoded.replaceAll(
+      /<\/script(?=[\t\n\f\r >\/]|$)/,
+      "<\/script",
+    );
+
+    return encoded;
   }
 
-  if (tag == 'style') {
-    
+  if (tag == "style") {
+    // This is safe if the type is CSS because the only location where this
+    // sequence can occur is in a string literal, where this will be valid
+    // and equivalent, or a comment.
+    encoded = encoded.replaceAll(
+      /<\/style(?=[\t\n\f\r >\/]|$)/,
+      "\\3C\/style",
+    );
+
+    return encoded;
   }
 
-  encoded = encoded.replaceAll('&', '&amp;');
+  if (tag == "!--") {
+    // https://html.spec.whatwg.org/#comments
+    encoded = encoded.replaceAll("<!--", "< !--");
+    encoded = encoded.replaceAll("-->", "-- >");
+    encoded = encoded.replaceAll("--!>", "-- !>");
+    encoded = encoded.replace(/^>/ug, " >");
+    encoded = encoded.replace(/^->/ug, "- >");
+    encoded = encoded.replace(/<!-$/ug, "< !-");
 
-  if (tag != 'textarea' && tag != 'title') {
-    encoded = encoded.replaceAll('<', '&gt;');
+    return encoded;
   }
 
-  encoded = encoded.replaceAll(']]>', ']]&lt;');
+  encoded = encoded.replace(/&(?=[a-zA-Z0-9]+;|[a-zA-Z0-9]*$)/ug, "&amp;");
+
+  encoded = encoded.replaceAll("]]>", "]]&gt;");
+
+  if (tag == "title") {
+    encoded = encoded.replaceAll("<![CDATA[", "&lt;![CDATA[");
+    encoded = encoded.replaceAll(
+      /<\/title(?=[\t\n\f\r >\/]|$)/,
+      "\\3C\/title",
+    );
+  } else if (tag == "textarea") {
+    encoded = encoded.replaceAll("<![CDATA[", "&lt;![CDATA[");
+    encoded = encoded.replaceAll(
+      /<\/textarea(?=[\t\n\f\r >\/]|$)/,
+      "\\3C\/textarea",
+    );
+  } else {
+    encoded = encoded.replaceAll("<", "&gt;");
+  }
 
   return encoded;
 }
-
-export function textToHtml
